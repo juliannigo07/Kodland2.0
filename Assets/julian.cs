4780aa83f4f88e7133678c21df8b9690b49d0b41
juliannigo07 <juliangomez0317@gmail.com>
2026-06-05 15:55:55 -0500

    05/06

---
+++ Assets/julian.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class julian : MonoBehaviour
+{
+    
+    public int EdadCarlos = 35; //Felipe
+
+    //BLOQUES DE CODIGO: SON CAJITAS PARA GUARDAR INSTRUCCIONES
+    void Start() //Se ejecuta una sola vez al iniciar el juego
+    {
+        int vidaJugador = 0; //MUerto
+        print("El nombre de mi estudiante es = " + EdadCarlos);
+    }
+
+    void Update()  //Se ejecuta cada frame, mayormente cada 60 veces por segundo, dependiendo de los FPS del juego
+    {
+        
+
+    }
+
+}
